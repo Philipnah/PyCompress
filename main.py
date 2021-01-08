7b67dffe0d7e3da972ec3df3106945ca7b862ff8
Philip Han <46556419+Philipnah@users.noreply.github.com>
2021-01-08 10:59:45 +0100

    added main file

---
+++ main.py
@@ -0,0 +1,28 @@
+from PIL import Image
+import os
+import glob
+
+filePath = input("File: ")
+filePath = filePath[1:-1]
+
+fileName = filePath.split("\\")[len(filePath.split("\\")) - 1]
+
+image = Image.open(filePath)
+
+size = image.size
+
+print("Current image size of " + fileName + " is: " + str(size))
+
+newSize = input("(Default is 300x168) New image size: ")
+if newSize == "\r" or newSize == "" or newSize == " ":
+     newSize = [300, 168]
+else:
+     "130 130"
+     newSize = newSize.split(" ")
+     newSize = [int(newSize[0]), int(newSize[1])]
+
+print("New size is " + "(" + str(newSize[0]) + ", " + str(newSize[1]) + ")")
+
+newImage = image.resize((newSize[0], newSize[1]), Image.ANTIALIAS)
+
+newImage.save("C:\\Users\\phili\\Desktop\\" + "Downscaled_" + fileName, optimize=True, quality=95)
